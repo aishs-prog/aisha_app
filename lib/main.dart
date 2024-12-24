bde3f286e31fe7c28bf47d81d045cd5d768a9334
Aya Alsakkaf <ayaalsakkaf340@gmail.com>
2024-12-24 19:50:01 +0000

    homework 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'validator.dart';
 
 void main() {
@@ -8,136 +9,132 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'نموذج تسجيل',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.light,
+        scaffoldBackgroundColor: Colors.white,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.blue,
+        ),
+      ),
+      darkTheme: ThemeData(
+        primarySwatch: Colors.blue,
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+        ),
+      ),
+      themeMode: ThemeMode.system,
       home: RegistrationForm(),
     );
   }
 }
 
-class RegistrationForm extends StatefulWidget {
-  @override
-  _RegistrationFormState createState() => _RegistrationFormState();
+class ThemeController extends GetxController {
+  var isDarkMode = false.obs;
+
+  void toggleTheme() {
+    isDarkMode.value = !isDarkMode.value;
+    Get.changeThemeMode(isDarkMode.value ? ThemeMode.dark : ThemeMode.light);
+  }
 }
 
-class _RegistrationFormState extends State<RegistrationForm> {
-  final _formKey = GlobalKey<FormState>();
-  String name = '';
-  String phone = '';
-  String email = '';
-  String password = '';
-  bool termsAccepted = false;
+class RegistrationForm extends StatelessWidget {
+  final ThemeController themeController = Get.put(ThemeController());
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [Colors.blue[900]!, Colors.cyan[600]!],
-                begin: Alignment.topLeft,
-                end: Alignment.bottomRight,
-              ),
-            ),
-          ),
-          Center(
-            child: ClipPath(
-              clipper: OvalShapeClipper(),
-              child: Container(
-                width: MediaQuery.of(context).size.width * 0.90,
-                height: MediaQuery.of(context).size.height * 0.6,
-                color: Colors.white,
-                child: SingleChildScrollView(
-                  padding: EdgeInsets.all(50),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Text(
-                          'تسجيل',
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue[900]),
-                        ),
-                        SizedBox(height: 10),
-                        _buildTextField(
-                          label: 'الاسم',
-                          icon: Icons.person,
-                          onChanged: (value) => name = value,
-                          validator: FormValidator.validateName,
-                        ),
-                        _buildTextField(
-                          label: 'رقم الهاتف',
-                          icon: Icons.phone,
-                          onChanged: (value) => phone = value,
-                          validator: FormValidator.validatePhone,
-                        ),
-                        _buildTextField(
-                          label: 'البريد الإلكتروني',
-                          icon: Icons.email,
-                          onChanged: (value) => email = value,
-                          validator: FormValidator.validateEmail,
-                        ),
-                        _buildTextField(
-                          label: 'كلمة المرور',
-                          icon: Icons.lock,
-                          obscureText: true,
-                          onChanged: (value) => password = value,
-                          validator: FormValidator.validatePassword,
-                        ),
-                        Row(
-                          children: [
-                            Checkbox(
-                              value: termsAccepted,
-                              onChanged: (value) {
-                                setState(() {
-                                  termsAccepted = value!;
-                                });
-                              },
-                            ),
-                            Expanded(
-                              child: Text('أوافق على الشروط والأحكام'),
-                            ),
-                          ],
-                        ),
-                        SizedBox(height: 15),
-                        ElevatedButton(
-                          onPressed: () {
-                            if (_formKey.currentState!.validate() &&
-                                termsAccepted) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(content: Text('تم التسجيل بنجاح!')),
-                              );
-                            } else if (!termsAccepted) {
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                SnackBar(
-                                    content: Text('يرجى قبول الشروط والأحكام')),
-                              );
-                            }
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 200, 209, 223),
-                            padding: EdgeInsets.symmetric(vertical: 15),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
-                          child: Text('تسجيل', style: TextStyle(fontSize: 18)),
-                        ),
-                      ],
+      appBar: AppBar(
+        title: Text('نموذج تسجيل'),
+        actions: [
+          Obx(() => IconButton(
+                icon: Icon(
+                  themeController.isDarkMode.value
+                      ? Icons.dark_mode
+                      : Icons.light_mode,
+                ),
+                onPressed: themeController.toggleTheme,
+              )),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.90,
+          height: MediaQuery.of(context).size.height * 0.6,
+          color: Theme.of(context).cardColor,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(50),
+            child: Form(
+              key: GlobalKey<FormState>(),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Text(
+                    'تسجيل',
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).textTheme.titleLarge!.color,
                     ),
                   ),
-                ),
+                  SizedBox(height: 10),
+                  _buildTextField(
+                    label: 'الاسم',
+                    icon: Icons.person,
+                    onChanged: (value) {},
+                    validator: FormValidator.validateName,
+                  ),
+                  _buildTextField(
+                    label: 'رقم الهاتف',
+                    icon: Icons.phone,
+                    onChanged: (value) {},
+                    validator: FormValidator.validatePhone,
+                  ),
+                  _buildTextField(
+                    label: 'البريد الإلكتروني',
+                    icon: Icons.email,
+                    onChanged: (value) {},
+                    validator: FormValidator.validateEmail,
+                  ),
+                  _buildTextField(
+                    label: 'كلمة المرور',
+                    icon: Icons.lock,
+                    obscureText: true,
+                    onChanged: (value) {},
+                    validator: FormValidator.validatePassword,
+                  ),
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: false,
+                        onChanged: (value) {},
+                      ),
+                      Expanded(
+                        child: Text('أوافق على الشروط والأحكام'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 15),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.green[700],
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                    child: Text('تسجيل', style: TextStyle(fontSize: 18)),
+                  ),
+                ],
               ),
             ),
           ),
-        ],
+        ),
       ),
     );
   }
@@ -152,29 +149,15 @@ class _RegistrationFormState extends State<RegistrationForm> {
     return TextFormField(
       decoration: InputDecoration(
         labelText: label,
-        prefixIcon: Icon(icon, color: Colors.blue[700]),
+        prefixIcon: Icon(icon, color: Colors.green[700]),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(10),
-          borderSide: BorderSide(color: Colors.blue[700]!),
+          borderSide: BorderSide(color: Colors.green[700]!),
         ),
       ),
       onChanged: onChanged,
       validator: validator,
       obscureText: obscureText,
     );
-  }
-}
-
-class OvalShapeClipper extends CustomClipper<Path> {
-  @override
-  Path getClip(Size size) {
-    var path = Path();
-    path.addOval(Rect.fromLTWH(0, 0, size.width, size.height));
-    return path;
-  }
-
-  @override
-  bool shouldReclip(CustomClipper<Path> oldClipper) {
-    return false;
   }
 }
